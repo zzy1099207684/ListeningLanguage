a843bd45ceee1039413f2a247bcf1e92b816d59d
zhiyo <1099207684@qq.com>
2025-01-26 10:50:39 +0200

    add some data

--- data_sql/setting.sql
+++ data_sql/setting.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.setting (id, name, selected_groups) VALUES (99, 'index_top', '["group1"]');
-INSERT INTO public.setting (id, name, selected_groups) VALUES (86, 'edit_choose', '["group1", "math", "computer"]');
 INSERT INTO public.setting (id, name, selected_groups) VALUES (88, 'combined_training', '["group1", "math", "computer"]');
+INSERT INTO public.setting (id, name, selected_groups) VALUES (86, 'edit_choose', '[]');
